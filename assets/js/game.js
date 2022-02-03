7e1a3a3873a8600d9e0b95d606983f9989677be9
nancywa1 <nancy.wang0717@gmail.com>
2022-02-02 21:14:38 -0700

    console log

---
+++ assets/js/game.js
@@ -0,0 +1,20 @@
+var playername=window.prompt("what is your robot's name");
+//Note the lack of quotation marks around playerName
+//window.alert(playername);
+//what is this
+console.log(playername);
+console.log("This logs a string, good for leaving yourself a message")
+// this will do match and log 20
+console.log(10+10);
+// what is this 
+console.log("Our robot's name is "+playername);
+var playername="Tony the Robot";
+console.log("Tony the Robot" + " is ready for battle")
+console.log(playername+" is ready for battle!")
+console.log("Your robot, " + playername +",has won!")
+function fight() {
+    window.alert("The fight has begun")
+}
+//fight();
+var name="your name";
+console.log (name);
